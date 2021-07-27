2f71b0f7c2e24b2ef610336acfc3f6eb71d90399
tacchi <tacchi.imas@gmail.com>
2021-07-27 12:01:23 +0900

    mod

--- class/NJ2/final.py
+++ class/NJ2/final.py
@@ -9,6 +9,23 @@ img = cv2.imread("元画像.jpg")
 rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 print(rgb_img)
 
+#R,G,Bごとに読み取り
+r, g, b = img[:,:,0], img[:,:,1], img[:,:,2]
+
+#ヒストグラム化
+hist_r = cv2.calcHist([r],[0],None,[256],[0,256])
+hist_g = cv2.calcHist([g],[0],None,[256],[0,256])
+hist_b = cv2.calcHist([b],[0],None,[256],[0,256])
+
+#ヒストグラム表示
+plt.plot(hist_r, color='r', label="r")
+plt.plot(hist_g, color='g', label="g")
+plt.plot(hist_b, color='b', label="b")
+plt.legend()
+plt.show()
+
+
+
 #HSVに変換
 hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-print(hsv_img)
+
